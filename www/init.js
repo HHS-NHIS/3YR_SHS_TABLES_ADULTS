8176c382126c1463db9b5b2afd25c348c9fc48bd
HHS-NHIS <52418796+HHS-NHIS@users.noreply.github.com>
2023-01-25 09:34:30 -0500

    First commit

---
+++ www/init.js
@@ -0,0 +1,126 @@
+var isPivot = false; var initCols = [
+                      { title: "Year", className: "sub", "visible": false},
+                      { title: "row_var", className: "sub", "visible": false},
+                      { title: "rowLevels" , className: "main"},
+                      { title: "rowLevNum" , className: "sub", "visible": false},
+                      { title: "selected",   className: "sub", "visible" : false},
+                      
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay},
+
+                  {title: "", className: "coef", searchable: false, render: coefDisplay},
+
+                {title: "", className: "se", searchable: false, render: seDisplay}]; var initLevels = {"asiandet":{"asiandetA":"Asian Indian","asiandetB":"Chinese","asiandetC":"Filipino","asiandetD":"Japanese","asiandetE":"Korean","asiandetF":"Vietnamese","asiandetG":"Other Asian"},"hisp":{"hispA":"Hispanic or Latino origin","hispB":"Not Hispanic or Latino origin"},"hispasian":{"hispasianA":"Hispanic Asian","hispasianB":"NH Asian Indian","hispasianC":"NH Chinese","hispasianD":"NH Filipino","hispasianE":"NH Japanese","hispasianF":"NH Korean","hispasianG":"NH Vietnamese","hispasianH":"NH Other Asian"},"hispdet":{"hispdetB":"Central American","hispdetc":"South American","hispdetD":"Puerto Rican","hispdetE":"Cuban","hispdetF":"Dominican","hispdetG":"Other Hispanic, Latino, or Spanish","hispdetA":"Mexican or Mexican American"},"hisprace":{"hispraceA":"Hispanic, any race","hispraceB":"NH White only","hispraceC":"NH Black only","hispraceD":"NH AIAN only","hispraceE":"NH Asian only","hispraceF":"NH NHPI only","hispraceG":"NH Black and White","hispraceH":"NH AIAN and White"},"race":{"raceA":"White, single race","raceB":"Black, single race","raceC":"AIAN, single race","raceD":"Asian, single race","raceE":"NHPI, single race"},"racethree":{"racethreeA":"White, single or multiple races","racethreeB":"Black, single or multiple races","racethreeC":"AIAN, single or multiple races","racethreeD":"Asian, single or multiple races","racethreeE":"NHPI, single or multiple races"},"racetwo":{"racetwoA":"Black and White","racetwoB":"AIAN and White"}}; var subLevels = ["Physician office visits","Non-physician office visits","Physician hosp. visits","Non-physician hosp. visits"];
